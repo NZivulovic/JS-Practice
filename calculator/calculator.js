daf6e3000ea54924988e6995c703d75f84f909e0
NZivulovic <104269774+NZivulovic@users.noreply.github.com>
2025-12-12 20:40:27 +0100

    calculator fundamental layout - html,css (no js functions yet)

---
+++ calculator/calculator.js
@@ -0,0 +1,20 @@
+function calc(num1, operator, num2) {
+    let resultCalculation = 0;
+    function operate() {
+        const calculation = 0;
+        if (operator === '+') {
+            calculation = num1 + num2;
+        }
+        if (operator === '-') {
+            calculation = num1 - num2;
+        }
+        if (operator === '*') {
+            calculation = num1 * num2;
+        }
+        if (operator === '/') {
+            calculation = num1 / num2;
+        }
+        return resultCalculation = calculation;
+    }
+    operate();
+}
